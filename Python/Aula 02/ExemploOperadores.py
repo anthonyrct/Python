1ab3d906b37e88e55c352f8ba27db1a796ca4cdc
anthonyrct <123770471+anthonyrct@users.noreply.github.com>
2024-02-07 16:52:40 -0300

    Add files via upload

---
+++ Python/Aula 02/ExemploOperadores.py
@@ -0,0 +1,42 @@
+# Exemplos:Operadores Aritimeticos
+
+a = 10
+b = 3
+
+soma = a + b
+subtracao = a - b
+multiplicacao = a * b
+divisao = a / b
+divisao_inteira = a // b
+resto = a % b
+potencia = a**b
+
+# Exemplos:Operadore Relacionais
+
+x = 5
+y = 10
+
+igual = x == y
+diferente = x != y
+menor = x < y
+maior = x > y
+menor_igual = x <= y
+maior_igual = x >= y
+
+
+# Exemplos: operaçoes Lógicos
+
+p = True
+q = False
+
+resultado_and = p and q
+resultado_or = p or q
+resultado_not = not p
+
+print(resultado_and, resultado_or, resultado_not)
+
+
+print(igual, diferente, menor, maior, menor_igual, maior_igual)
+
+
+print(soma, subtracao, multiplicacao, divisao, divisao_inteira, resto, potencia)
---
+++ Python/Exercicios 01/manipulacao_de_string.py
@@ -0,0 +1,6 @@
+nome = "Roger"
+sobrenome = "Mendes"
+nome_completo = nome + "" + sobrenome
+print(nome_completo)
+print("Nome Completo em Minusculas:", nome_completo.lower())
+print("Nome Completo em Maiusculas:", nome_completo.upper())
