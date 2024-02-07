f0014cc2ae795609e91e3341b0aa6485225166b9
anthonyrct <123770471+anthonyrct@users.noreply.github.com>
2024-02-07 16:52:00 -0300

    Add files via upload

---
+++ Exercicios 01/Variaveis_e_Operacoes_com_Numeros.py
@@ -0,0 +1,12 @@
+raio_circulo = 5
+area_circulo = (raio_circulo * 3.14)**2
+
+soma = raio_circulo + area_circulo
+subtracao = raio_circulo - area_circulo
+multiplicacao = raio_circulo * area_circulo
+divisao = raio_circulo / area_circulo
+divisao_inteira = raio_circulo // area_circulo
+resto = raio_circulo % area_circulo
+potencia = raio_circulo ** area_circulo
+
+print(soma, subtracao, multiplicacao, divisao, divisao_inteira, resto, potencia)
